ff697a0f47080b1ccfb1496d8bedcecaa7b70d63
hafsa <abdullahihafsat585@gmail.com>
2025-12-12 15:44:40 +0100

    .

--- SnoozeNot_project/settings.py
+++ SnoozeNot_project/settings.py
@@ -123,10 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # For development/testing purposes
-EMAIL_HOST = 'smtp.email.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yourgmail.gmail.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
